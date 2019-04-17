88302862232b467780f084a80c0b513a6cf606f3
Deepak Bhari <deepak.bhari1@Deepaks-MBP.lan>
2019-04-17 13:47:35 +0100

    formatting jsx - differences with html

--- jsx/src/index.js
+++ jsx/src/index.js
@@ -4,7 +4,21 @@ import ReactDOM from "react-dom";
 
 // Create a react component (function based)
 const App = () => {
-  return <div>Hi there!</div>;
+    const buttonText = {text: 'Click Me!'};
+    const style = { backgroundColor: "blue", color: "white" };
+    const labelText = "Enter name:";
+
+  return (
+    <div>
+      <label className="label" htmlFor="name">
+        {labelText}
+      </label>
+      <input id="name" type="text" />
+      <button style={style}>
+        {buttonText.text}
+      </button>
+    </div>
+  );
 };
 
 // Take the react component and show it on the screen
