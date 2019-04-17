475c2cc5f2d123d35954247b80b7aab07b1b2f59
Deepak Bhari <deepak.bhari1@Deepaks-MBP.lan>
2019-04-17 22:32:49 +0100

    updating state properties with setState and triggering rerender

--- seasons/src/index.js
+++ seasons/src/index.js
@@ -7,18 +7,21 @@ class App extends React.Component{
         //super is a reference to the parents constructor and has to be called
         super(props); 
 
+        //this is the only time we do direct assignment to this.state
         this.state = { lat: null };
+
+        window.navigator.geolocation.getCurrentPosition(
+            (position) => { 
+                this.setState({ lat: position.coords.latitude }) 
+            },
+            (error) => console.log(error)
+        );
     }
 
     // React says we have to define render!
     render(){
-        window.navigator.geolocation.getCurrentPosition(
-            (position) => console.log(position),
-            (error) => console.log(error)
-        );
-
         return(
-            <div>Latitude: </div>
+            <div>Latitude: {this.state.lat}</div>
         );
     }
 }
