74e21443ec054228df38971f2b5a764f8143ba16
Deepak Bhari <deepak.bhari1@Deepaks-MBP.lan>
2019-04-17 19:15:44 +0100

    pass CommentDetail as a child component into ApprovalCard - it then becomes available in ApprovalCard from props.children - the child component

--- components/src/index.js
+++ components/src/index.js
@@ -11,13 +11,15 @@ import ApprovalCard from "./ApprovalCard";
 const App = () => {
   return (
     <div className="ui container comments">
-      <ApprovalCard />
-      <CommentDetail
-        author="Sam"
-        timeAgo="Today at 4:45PM"
-        content="Nice blog"
-        avatar={faker.image.avatar()}
-      />
+      <ApprovalCard>
+        <CommentDetail
+          author="Sam"
+          timeAgo="Today at 4:45PM"
+          content="Nice blog"
+          avatar={faker.image.avatar()}
+        />
+      </ApprovalCard>
+
       <CommentDetail
         author="Alex"
         timeAgo="Today at 2:00AM"
