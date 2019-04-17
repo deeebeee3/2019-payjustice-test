d871ac80f179dbd91bd262dd22e6dd7720b6c7b7
Deepak Bhari <deepak.bhari1@Deepaks-MBP.lan>
2019-04-17 22:24:46 +0100

    initializing state through constructors

--- seasons/src/index.js
+++ seasons/src/index.js
@@ -2,8 +2,16 @@ import React from "react";
 import ReactDOM from "react-dom";
 
 class App extends React.Component{
-    render(){
 
+    constructor(props){
+        //super is a reference to the parents constructor and has to be called
+        super(props); 
+
+        this.state = { lat: null };
+    }
+
+    // React says we have to define render!
+    render(){
         window.navigator.geolocation.getCurrentPosition(
             (position) => console.log(position),
             (error) => console.log(error)
