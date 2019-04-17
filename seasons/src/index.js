5a851410a72676f0c288b4bf442b14ca3c95b63f
Deepak Bhari <deepak.bhari1@Deepaks-MBP.lan>
2019-04-17 20:54:59 +0100

    getting a users physical location

--- seasons/src/index.js
+++ seasons/src/index.js
@@ -2,6 +2,11 @@ import React from "react";
 import ReactDOM from "react-dom";
 
 const App = () => {
+    window.navigator.geolocation.getCurrentPosition(
+        (position) => console.log(position),
+        (error) => console.log(error)
+    );
+
     return(
         <div>Hi there!</div>
     );
