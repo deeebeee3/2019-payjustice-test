6fac37b3a52d69ebf1ccb1dbf3636cfb3c22b45b
Deepak Bhari <deepak.bhari1@Deepaks-MBP.lan>
2019-04-17 22:48:29 +0100

    handling errors gracefully

--- seasons/src/index.js
+++ seasons/src/index.js
@@ -1,32 +1,37 @@
 import React from "react";
 import ReactDOM from "react-dom";
 
-class App extends React.Component{
+class App extends React.Component {
+  constructor(props) {
+    //super is a reference to the parents constructor and has to be called
+    super(props);
 
-    constructor(props){
-        //super is a reference to the parents constructor and has to be called
-        super(props); 
+    //this is the only time we do direct assignment to this.state
+    this.state = {
+      lat: null,
+      errorMessage: ""
+    };
 
-        //this is the only time we do direct assignment to this.state
-        this.state = { lat: null };
+    window.navigator.geolocation.getCurrentPosition(
+      position => {
+        this.setState({ lat: position.coords.latitude });
+      },
+      error => {
+          this.setState({ errorMessage: error.message });
+      }
+    );
+  }
 
-        window.navigator.geolocation.getCurrentPosition(
-            (position) => { 
-                this.setState({ lat: position.coords.latitude }) 
-            },
-            (error) => console.log(error)
-        );
-    }
-
-    // React says we have to define render!
-    render(){
-        return(
-            <div>Latitude: {this.state.lat}</div>
-        );
-    }
+  // React says we have to define render!
+  render() {
+    return (
+        <div>
+            Latitude: {this.state.lat}
+            <br />
+            Error: {this.state.errorMessage}
+        </div>
+    );
+  }
 }
 
-ReactDOM.render(
-    <App />,
-    document.querySelector('#root')
-);
+ReactDOM.render(<App />, document.querySelector("#root"));
