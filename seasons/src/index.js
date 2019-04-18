dd63c6c650504c374bf6a02fe32677eddd03f729
Deepak Bhari <deepak.bhari1@Deepaks-MBP.lan>
2019-04-18 01:19:51 +0100

    avoiding conditionals in render and putting them into seperate function

--- seasons/src/index.js
+++ seasons/src/index.js
@@ -1,16 +1,16 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import SeasonDisplay from './SeasonDisplay';
-import Spinner from './Spinner';
+import React from "react";
+import ReactDOM from "react-dom";
+import SeasonDisplay from "./SeasonDisplay";
+import Spinner from "./Spinner";
 
 class App extends React.Component {
-//   constructor(props) {
-//     //super is a reference to the parents constructor and has to be called
-//     super(props);
+  //   constructor(props) {
+  //     //super is a reference to the parents constructor and has to be called
+  //     super(props);
 
-//     //this is the only time we do direct assignment to this.state
-//     this.state = { lat: null, errorMessage: "" };
-//   }
+  //     //this is the only time we do direct assignment to this.state
+  //     this.state = { lat: null, errorMessage: "" };
+  //   }
 
   state = { lat: null, errorMessage: "" };
 
@@ -26,17 +26,20 @@ class App extends React.Component {
     console.log("My component was just updated - it rerendered");
   }
 
-  // React says we have to define render!
-  render() {
+  renderContent() {
     if (this.state.errorMessage && !this.state.lat) {
       return <div>Error: {this.state.errorMessage}</div>;
     }
 
     if (!this.state.errorMessage && this.state.lat) {
-        return <SeasonDisplay lat={this.state.lat} />
+      return <SeasonDisplay lat={this.state.lat} />;
     }
 
     return <Spinner loadingMsg="Please accept location request" />;
+  }
+
+  render() {
+    return <div className="border red">{this.renderContent()}</div>;
   }
 }
 
