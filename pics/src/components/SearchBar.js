6a3df5dd9e36a7933214c40e53010c42a7d3bf8d
Deepak Bhari <deepak.bhari1@Deepaks-MBP.lan>
2019-04-19 08:13:42 +0100

    search bar project scaffolding

---
+++ pics/src/components/SearchBar.js
@@ -0,0 +1,23 @@
+import React from "react";
+
+class SearchBar extends React.Component {
+  onInputChange(event) {
+    console.log(event.target.value);
+  }
+
+  render() {
+    return (
+      <div className="ui segment">
+        <form className="ui form">
+          <div className="field">
+            <label>Image Search</label>
+            {/* onInputChange is a callback function - dont use parentheses - dont want to execute on render */}
+            <input type="text" onChange={this.onInputChange}/> 
+          </div>
+        </form>
+      </div>
+    );
+  }
+}
+
+export default SearchBar;
