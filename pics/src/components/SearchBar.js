824f012bdb19023452ea266d353296e1cbc9dc1c
Deepak Bhari <deepak.bhari1@Deepaks-MBP.lan>
2019-04-19 08:28:36 +0100

    controlled element

--- pics/src/components/SearchBar.js
+++ pics/src/components/SearchBar.js
@@ -1,9 +1,7 @@
 import React from "react";
 
 class SearchBar extends React.Component {
-  onInputChange(event) {
-    console.log(event.target.value);
-  }
+  state = { term: "" };
 
   render() {
     return (
@@ -11,12 +9,11 @@ class SearchBar extends React.Component {
         <form className="ui form">
           <div className="field">
             <label>Image Search</label>
-            
-            {/* onInputChange is a callback function - dont use parentheses - dont want to execute on render */}
-            <input type="text" onChange={this.onInputChange} />
-
-            {/* alternate way */}
-            {/* <input type="text" onChange={(e) => console.log(e.target.value)} /> */}
+            <input
+              type="text"
+              value={this.state.term}
+              onChange={e => this.setState({ term: e.target.value })}
+            />
           </div>
         </form>
       </div>
