e15527fa3093ce5532dc4126f1536594389061a4
Deepak Bhari <deepak.bhari1@deepaks-mbp.home>
2019-04-21 20:22:31 +0100

    Accessing the DOM with Refs - define constructor function and create reference there which we can use in class, add ref to image tag so we can now have access to DOM info for imgtag

--- pics/src/components/ImageCard.js
+++ pics/src/components/ImageCard.js
@@ -3,13 +3,16 @@ import React from "react";
 class ImageCard extends React.Component {
   constructor(props) {
     super(props);
+
+    //create a reference and assign it to an instance variable we can use in our class
+    this.imageRef = React.createRef();
   }
 
   render() {
     const { description, urls } = this.props.image;
     return (
       <div>
-        <img alt={description} src={urls.regular} />
+        <img ref={this.imageRef} alt={description} src={urls.regular} />
       </div>
     );
   }
