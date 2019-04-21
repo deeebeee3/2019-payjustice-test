6a2419bd64d3669d268aa9281b41a6766d074102
Deepak Bhari <deepak.bhari1@deepaks-mbp.home>
2019-04-21 18:50:02 +0100

    implementing keys in lists - makes updating the DOM much more efficient when rendering lists

--- pics/src/components/ImageList.js
+++ pics/src/components/ImageList.js
@@ -3,7 +3,9 @@ import React from "react";
 const ImageList = props => {
   //use map to create an array of img tags
   const images = props.images.map(image => {
-    return <img src={image.urls.regular} />;
+    //when rendering lists - the root tag should have a unique key property
+    //so if img was wrapped in a div - the key property should be on the div tag and not the img
+    return <img alt={image.description} key={image.id} src={image.urls.regular} />;
   });
 
   return <div>{images}</div>;
