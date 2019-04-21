73d926f9b58a7f4bec8776ba5d712bf558fb785f
Deepak Bhari <deepak.bhari1@deepaks-mbp.home>
2019-04-21 16:17:11 +0100

    fetching data with axios and viewing request results in network tab of browser - only interested in second request with GET method

--- pics/src/components/App.js
+++ pics/src/components/App.js
@@ -1,18 +1,24 @@
 import React from "react";
+import axios from "axios";
 import SearchBar from "./SearchBar";
 
 // refactor App function into class based component so it can have some function it can
 // take and pass down into the SearchBar component
 class App extends React.Component {
-
-  onSearchSubmit(term){
-    console.log(term);
+  onSearchSubmit(term) {
+    axios.get("https://api.unsplash.com/search/photos", {
+      params: { query: term },
+      headers: {
+        Authorization:
+          "Client-ID 4350aad5e239db395b9a15b70618b7adbde58196ac64bbdec7c8af1c5f0ae895"
+      }
+    });
   }
 
   render() {
     return (
       <div className="ui container" style={{ marginTop: "10px" }}>
-        <SearchBar onSubmit={ this.onSearchSubmit }/>
+        <SearchBar onSubmit={this.onSearchSubmit} />
       </div>
     );
   }
