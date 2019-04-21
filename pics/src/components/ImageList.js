15caa53c8b803aeb3f60fa8011a172a0f347fb09
Deepak Bhari <deepak.bhari1@deepaks-mbp.home>
2019-04-21 18:53:40 +0100

    destructure (image) into ({description, id, urls}) and use in img tag

--- pics/src/components/ImageList.js
+++ pics/src/components/ImageList.js
@@ -1,11 +1,13 @@
 import React from "react";
 
 const ImageList = props => {
-  //use map to create an array of img tags
-  const images = props.images.map(image => {
+  //destructure (image) into ({description, id, urls})
+  const images = props.images.map(({ description, id, urls }) => {
     //when rendering lists - the root tag should have a unique key property
     //so if img was wrapped in a div - the key property should be on the div tag and not the img
-    return <img alt={image.description} key={image.id} src={image.urls.regular} />;
+    return (
+      <img alt={description} key={id} src={urls.regular} />
+    );
   });
 
   return <div>{images}</div>;
