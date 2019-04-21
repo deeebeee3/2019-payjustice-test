86e4635d2c6504813044e5f312e104a30f2e353e
Deepak Bhari <deepak.bhari1@deepaks-mbp.home>
2019-04-21 16:37:49 +0100

    setting state after async requests - error...

--- pics/src/components/App.js
+++ pics/src/components/App.js
@@ -2,9 +2,14 @@ import React from "react";
 import axios from "axios";
 import SearchBar from "./SearchBar";
 
-// refactor App function into class based component so it can have some function it can
-// take and pass down into the SearchBar component
 class App extends React.Component {
+
+  //expecting an array from request so use an empty array as default value
+  //also can check for array length. If used null, map would give an error message, and length too
+  state = { images: [] };
+
+
+
   async onSearchSubmit(term) {
     const response = await axios.get("https://api.unsplash.com/search/photos", {
       params: { query: term },
@@ -14,13 +19,15 @@ class App extends React.Component {
       }
     });
 
-    console.log(response.data.results);
+    //after get response - set state with images and cause component to rerender
+    this.setState({ images: response.data.results });
   }
 
   render() {
     return (
       <div className="ui container" style={{ marginTop: "10px" }}>
         <SearchBar onSubmit={this.onSearchSubmit} />
+        Found: {this.state.images.length} images
       </div>
     );
   }
