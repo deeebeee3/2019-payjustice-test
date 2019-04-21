81752d21de958cfb91da702410f1f4e3533ab166
Deepak Bhari <deepak.bhari1@deepaks-mbp.home>
2019-04-21 21:17:46 +0100

    how we got dynamic spans - steps involved in using refs

--- pics/src/components/ImageCard.js
+++ pics/src/components/ImageCard.js
@@ -4,25 +4,33 @@ class ImageCard extends React.Component {
   constructor(props) {
     super(props);
 
-    //create a reference and assign it to an instance variable we can use in our class
-    this.imageRef = React.createRef();
+    this.state = { spans: 0 };
+
+    this.imageRef = React.createRef(); //step 1
   }
 
   componentDidMount() {
-    //listen for image load successful event
-    this.imageRef.current.addEventListener("load", this.setSpans);
+    this.imageRef.current.addEventListener("load", this.setSpans); //step 3
   }
 
   //use the image height to set the span for grid-row-end css
-  //context will be lost so make sure setSpan defined using an arrow function
+  //context will be lost ('this' is undefined) so make sure setSpan defined using an arrow function
   setSpans = () => {
-    console.log(this.imageRef.current.clientHeight);
+    //steps 4
+    const height = this.imageRef.current.clientHeight;
+
+    const spans = Math.ceil(height / 10);
+
+    this.setState({ spans: spans });
   };
 
   render() {
     const { description, urls } = this.props.image;
+
     return (
-      <div>
+      // step 5
+      <div style={{ gridRowEnd: `span ${this.state.spans}` }}>
+        {/* step 2 */}
         <img ref={this.imageRef} alt={description} src={urls.regular} />
       </div>
     );
